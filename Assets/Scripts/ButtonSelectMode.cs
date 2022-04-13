a7a1deb0aad408d534c174ee5625a0bf492f452f
AnonimBezDeneg <AnonimBezDeneg@gmail.com>
2022-04-13 20:03:38 +0300

    0.8

--- Assets/Scripts/ButtonSelectMode.cs
+++ Assets/Scripts/ButtonSelectMode.cs
@@ -7,16 +7,6 @@ public class ButtonSelectMode : MonoBehaviour
 {
     public int ModeID;
     // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void ModeSelect()
     {
